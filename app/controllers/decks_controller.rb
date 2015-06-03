88c305463b4d8ab0686215b34fb1b8b4a5e7050d
mgherm <herman@cictr.com>
2015-06-02 21:37:33 -0400

    Strong parameters workarounds

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -3,9 +3,15 @@ class DecksController < ApplicationController
     @decks = Deck.all
   end
   def show
-    @deck = Deck.find(params["id"])
+    @deck = Deck.find(params[:id])
   end
   def new
+
     @deck = Deck.new
+  end
+  def create
+    @deck = Deck.new(params.require(:deck).permit(:name))
+    @deck.save
+    redirect_to "/decks"
   end
 end
