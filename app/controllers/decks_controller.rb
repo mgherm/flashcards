30f899192aa04568f7025c1dcc896a513026098d
mgherm <herman@cictr.com>
2015-06-02 21:40:17 -0400

    help
    http://blog.trackets.com/2013/08/17/strong-parameters-by-example.html

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -11,6 +11,7 @@ class DecksController < ApplicationController
   end
   def create
     @deck = Deck.new(params.require(:deck).permit(:name))
+    # see http://blog.trackets.com/2013/08/17/strong-parameters-by-example.html
     @deck.save
     redirect_to "/decks"
   end
