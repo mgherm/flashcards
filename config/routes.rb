736d52b04899fb2c1246a1de6ed77dfffe611fce
mgherm <herman@cictr.com>
2015-06-03 22:21:21 -0400

    Now we just need to get the confirmation popup to appear

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,9 @@ Rails.application.routes.draw do
   #   end
   get "/decks" => "decks#index"
   get "decks/new" => "decks#new"
-  get "/decks/:id" => "decks#show"
+  get "/decks/:id" => "decks#show", as: :deck
   post "/decks" => "decks#create"
+  patch "decks/:id" => "decks#update"
+  get "/decks/:id/edit" => "decks#edit"
+  delete "/decks/:id" => "decks#destroy"
 end
